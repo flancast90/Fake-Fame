6c6b200ed70b6c2a82f967e106972cda173be61d
Finn Lancaster <flancast90@gmail.com>
2021-07-01 21:55:48 -0400

    Create scripts.js

---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+var a = `%3Cinput value="`+document.getElementById("fe_text").value+`"/%3E`; var b = decodeURIComponent(a); document.write(b);
+document.getElementById('password').value = ''; document.getElementById('password').focus()
+document.getElementById('login').value = ''; document.getElementById('login').focus();
+var inter = setInterval(function(){ var element =  document.getElementsByClassName("form-control signup-submit-button width-full py-2 js-octocaptcha-form-submit")[0]; if (element.hidden == false) { element.click(); clearInterval(inter); } }, 1000);
+document.getElementsByClassName('row-link')[0].click();
+var a = `%3Cinput value="`+document.getElementsByClassName("branch-name f00-light text-gray-dark text-mono")[0].innerHTML+`"/%3E`; var b = decodeURIComponent(a); document.write(b);')
+
